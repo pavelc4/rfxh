127e7b366d4e23003ae93ccdf669d30b1b76b619
Pavelc4 <pavelc4@users.noreply.github.com>
2026-07-10 22:35:28 +0700

    feat(text): create utf8.cpp, move impls from header

--- src/text/utf8.hpp
+++ src/text/utf8.hpp
@@ -2,26 +2,7 @@
 
 namespace rfxh::text {
 
-    static int utf8_char_len(unsigned char c) {
-      if (c < 0x80)
-        return 1;
-      if ((c & 0xE0) == 0xC0)
-        return 2;
-      if ((c & 0xF0) == 0xE0)
-        return 3;
-      if ((c & 0xF8) == 0xF0)
-        return 4;
-      return 1;
-    }
+int utf8_char_len(unsigned char c);
+int skip_ansi(const char* p);
 
-    static int skip_ansi(const char *p) {
-      if (p[0] != '\033' || p[1] != '[')
-        return 0;
-      int i = 2;
-      while (p[i] && ((p[i] >= '0' && p[i] <= '9') || p[i] == ';'))
-        i++;
-      if (p[i])
-        i++;
-      return i;
-    }
-}
+} // namespace rfxh::text
