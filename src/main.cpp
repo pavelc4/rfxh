33a1854647693102e78b2f609a8d6d4a96fec30f
Pavelc4 <pavelc4@users.noreply.github.com>
2026-07-10 22:53:38 +0700

    feat(main): rewrite entry point to use App::run

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
-#include "terminal/terminal.hpp"
-#include <iostream>
-#include <unistd.h>
+#include "engine/app.hpp"
 
-int main() {
-    rfxh::terminal::install_signal_handlers();
-    rfxh::terminal::RawModeGuard raw_mode;
-
-    std::cout << "press ctrl+c to test signal cleanup, or resize terminal\n";
-    for (int i = 0; i < 20; i++) {
-        if (rfxh::terminal::consume_resize_flag())
-            std::cout << "resize detected!\n";
-        sleep(1);
-    }
+int main(int argc, char** argv) {
+    rfxh::engine::App app;
+    app.run(argc, argv);
     return 0;
 }
