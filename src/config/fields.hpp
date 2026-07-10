b08eae4486267c83e94f061f707a53ffa472a155
Pavelc4 <pavelc4@users.noreply.github.com>
2026-07-10 23:06:13 +0700

    feat(config): add Field enum for system info fields

---
+++ src/config/fields.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace rfxh::config {
+
+    enum Field {
+        F_OS, F_HOST, F_KERNEL, F_UPTIME, F_PACKAGES,
+        F_SHELL, F_DISPLAY, F_WM, F_THEME, F_ICONS,
+        F_FONT, F_TERMINAL, F_CPU, F_GPU, F_MEMORY,
+        F_SWAP, F_DISK, F_IP, F_BATTERY, F_LOCALE,
+        F_COLORS, F_COUNT
+    };
+
+} // namespace rfxh::config
